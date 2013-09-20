4272f93c4a718d2f05efbf0e4e6e16bd3698569b
Jon H.M. Chan <me@jonhmchan.com>
2013-09-20 00:08:55 -0400

    Testing a cache swap to post changes immediately using pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,21 @@
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      // Swap it in and reload the page to get the new hotness.
+      window.applicationCache.swapCache();
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
+
 var app = angular.module('myApp', []);
 
 /* Change the interpolate provider so it doesn't conflict with tornado template engine */
