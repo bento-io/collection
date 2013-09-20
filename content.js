2f9d8a653dd31683136a447098ebf5d6699079c2
Jon H.M. Chan <me@jonhmchan.com>
2013-09-20 15:58:32 -0400

    Added java resource manually

--- content.js
+++ content.js
@@ -554,6 +554,10 @@ var CONTENT = [
       home: "",
       $then: ["python", "ruby", "node"],
       links: [
+      	{
+      		name: "Programming Methodology",
+      		url: "http://see.stanford.edu/see/courseinfo.aspx?coll=824a47e1-135f-4508-a5aa-866adcae1111"
+      	}
       ]
     },
     {
